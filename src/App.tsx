268c21e94f108d86e60a1788a71af420487542ac
felipe-kuhne <felipe.kuhne@hp.com>
2023-11-28 21:00:21 -0300

    removing unnecessary state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  setCount(1);
-
   return (
     <>
       <h1>Pra Crianca Ler</h1>
